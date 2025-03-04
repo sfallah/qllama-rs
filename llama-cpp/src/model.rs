d3a23d7985eeb25cc3ca35c335016a3848ac48b4
Saba Fallah <10401143+sfallah@users.noreply.github.com>
2025-03-04 13:27:21 +0100

    taken care of all warning minor changes

--- llama-cpp/src/model.rs
+++ llama-cpp/src/model.rs
@@ -93,6 +93,7 @@ impl LlamaChatMessage {
 }
 
 /// The Rope type that's used within the model.
+#[allow(missing_docs)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum RopeType {
     Norm,
@@ -482,6 +483,13 @@ impl LlamaModel {
         // It's never possible for this to panic because while the API interface is defined as an int32_t,
         // the field it's accessing is a uint32_t.
         u32::try_from(unsafe { llama_cpp_sys::llama_model_n_head(self.model.as_ptr()) }).unwrap()
+    }
+
+    /// Returns the number of KV attention heads.
+    pub fn n_head_kv(&self) -> u32 {
+        // It's never possible for this to panic because while the API interface is defined as an int32_t,
+        // the field it's accessing is a uint32_t.
+        u32::try_from(unsafe { llama_cpp_sys::llama_model_n_head_kv(self.model.as_ptr()) }).unwrap()
     }
 
     /// Returns the rope type of the model.
