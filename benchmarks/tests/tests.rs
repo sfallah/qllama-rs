12d7c24852f41ff0eed7ab86907bb50f0123804d
Saba Fallah <10401143+sfallah@users.noreply.github.com>
2025-10-14 15:01:59 +0200

    minor

--- benchmarks/tests/tests.rs
+++ benchmarks/tests/tests.rs
@@ -344,7 +344,7 @@ mod tetes {
         //let model_path = "models/embeddinggemma-300m-f16.gguf".to_string();
         //let model_path = "models/embeddinggemma-300M-BF16.gguf".to_string();
         //let model_path = "models/embeddinggemma-300m-with-st-denses-Q4_K_M.gguf".to_string();
-        let model_path = "models/embeddinggemma-300m-qat-q4_0-unquantized_with_st_denses-Q4_0.gguf".to_string();
+        let model_path = "models/embeddinggemma-300m-sentence-transformers-qat-q4_0.gguf".to_string();
 
 
 
